690b26dd9a0ee7b1b6fc5fc24f6edca7dc05fbbb
Samuel Naranjo <snaran1@students.towson.edu>
2018-12-09 17:57:09 -0500

    changes to nav

--- aimbuddy-react/src/components/header/header.jsx
+++ aimbuddy-react/src/components/header/header.jsx
@@ -1,40 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './header.css';
 
-class Header extends Component {
-
-    changeButtons() {
-        if(this.props.isSignedIn === false) {
-            return(
-                <React.Fragment>
-                    <button className="sign-in" onClick={this.props.handleLogInClick}>Log-In</button>
-                    <button className="register" onClick={this.props.handleRegisterClick}>Register</button>
-                </React.Fragment>
-            )
-        } else {
-            return (
-                <React.Fragment>
-                    <button className="home">Home</button>
-                </React.Fragment>
-            )
-        }
-    };
-
-    render() {
-        return (
+const Header = ({isSignedIn, handleLogInClick, handleRegisterClick, handleSidebarClick}) => {
+    if(isSignedIn === false) {
+        return(
+            <Fragment>
             <div className="heading-box">
-                <div className="left-hand">
-                    <button className="sidebar-button" onClick={this.props.handleSidebarClick}>=</button>
-                    <h1 style={{fontSize: 40, fontFamily: 'Georgia', display: 'inline-block',
-                        paddingLeft: 10, verticalAlign: 'middle', color: '#f7fdf7'}}>Grocer-mart</h1>
-                </div>
-                <div className='buttons'>
-                    {this.changeButtons()}
-                </div>
+            <div className="left-hand">
+               <button className="sidebar-button" onClick={handleSidebarClick}>=</button> 
+               <h1 style={{fontSize: 40, fontFamily: 'Georgia', display: 'inline-block',
+                        paddingLeft: 10, verticalAlign: 'middle', color: '#f7fdf7'}}>Grocer-mart
+                </h1>
+                
             </div>
-        );
+            <div className='buttons'>
+                <button className="sign-in" onClick={handleLogInClick}>Log-In</button>
+                <button className="register" onClick={handleRegisterClick}>Register</button>
+           
+            </div>
+            </div>
+            
+            </Fragment>
+        )
+    } else {
+        return(
+            <div>
+                <h1 style={{fontSize: 40, fontFamily: 'Georgia', display: 'inline-block',
+                        paddingLeft: 10, verticalAlign: 'middle', color: '#f7fdf7'}}>Grocer-mart
+                </h1>
+            </div>
+        )
     }
-
 }
 
 export default Header;
